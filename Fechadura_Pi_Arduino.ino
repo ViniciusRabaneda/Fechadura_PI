09327dfe3fb446ab9d3fafbb152c8b4c800996d1
Vinicius Rabaneda <vinirabaneda@hotmail.com>
2024-01-27 23:13:54 -0300

    Projeto PI finalizado

---
+++ Fechadura_Pi_Arduino.ino
@@ -0,0 +1,385 @@
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+int tempo = 180;
+int melody[] = {
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  
+  REST,8, NOTE_E5,8
+};
+
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int wholenote = (60000 * 4) / tempo;
+int divider = 0, noteDuration = 0;
+
+/* Detects patterns of knocks and triggers a motor to unlock it if the pattern is correct.
+  
+   Analog Pin 0: Piezo speaker (connected to ground with 1M pulldown resistor)
+   Digital Pin 2: Switch to enter a new code.  Short this to enter programming mode.
+   Digital Pin 3: DC gear reduction motor attached to the lock. (Or a motor controller or a solenoid or other unlocking mechanisim.)
+   Digital Pin 4: Red LED. 
+   Digital Pin 5: Green LED. 
+   Digital Pin 6: Buzzer.
+ */
+
+#include <Stepper.h>
+ 
+// Pin definitions
+const int knockSensor = A0;         // Piezo sensor on pin 0.
+const int programSwitch = 2;       // If this is high we program a new code.
+const int stepsPerRevolution = 2048;
+Stepper lockMotor (stepsPerRevolution, 8, 10, 9, 11); // Stepper motor used to turn the lock.
+const int redLED = 4;              // Status LED
+const int greenLED = 5;            // Status LED
+const int buzzer = 6;              // Buzzer
+ 
+// Tuning constants.  Could be made vars and hoooked to potentiometers for soft configuration, etc.
+const int threshold = 50;           // Minimum signal from the piezo to register as a knock
+const int rejectValue = 25;        // If an individual knock is off by this percentage of a knock we don't unlock..
+const int averageRejectValue = 15; // If the average timing of the knocks is off by this percent we don't unlock.
+const int knockFadeTime = 150;     // milliseconds we allow a knock to fade before we listen for another one. (Debounce timer.)
+const int lockTurnTime = 650;      // milliseconds that we run the motor to get it to go a half turn.
+
+const int maximumKnocks = 20;       // Maximum number of knocks to listen for.
+const int knockComplete = 1200;     // Longest time to wait for a knock before we assume that it's finished.
+
+// Variables.
+int secretCode[maximumKnocks] = {50, 25, 25, 50, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};  // Initial setup: "Shave and a Hair Cut, two bits."
+int knockReadings[maximumKnocks];   // When someone knocks this array fills with delays between knocks.
+int knockSensorValue = 0;           // Last reading of the knock sensor.
+int programButtonPressed = false;   // Flag so we remember the programming button setting at the end of the cycle.
+int open = false;
+
+void setup() {
+  
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(programSwitch, INPUT);
+  lockMotor.setSpeed(10);
+
+  Serial.begin(9600);               			// Uncomment the Serial.bla lines for debugging.
+  Serial.println("Program start.");  			// but feel free to comment them out after it's working right.
+  
+  digitalWrite(greenLED, HIGH);      // Green LED on, everything is go.
+}
+
+void loop() {
+  // Listen for any knock at all.
+  knockSensorValue = analogRead(knockSensor);
+
+  if (digitalRead(programSwitch)==HIGH){  // is the program button pressed?
+    programButtonPressed = true;          // Yes, so lets save that state
+    digitalWrite(redLED, HIGH);           // and turn on the red light too so we know we're programming.
+  } else {
+    programButtonPressed = false;
+    digitalWrite(redLED, LOW);
+  }
+  
+  if (knockSensorValue >=threshold){
+    listenToSecretKnock();
+  }
+
+  if (open == true && digitalRead(programSwitch)==HIGH) {
+    doorLock();
+  }
+
+} 
+
+// Records the timing of knocks.
+void listenToSecretKnock(){
+  Serial.println("knock starting");   
+
+  int i = 0;
+  // First lets reset the listening array.
+  for (i=0;i<maximumKnocks;i++){
+    knockReadings[i]=0;
+  }
+  
+  int currentKnockNumber=0;         			// Incrementer for the array.
+  int startTime=millis();           			// Reference for when this knock started.
+  int now=millis();
+  
+  digitalWrite(greenLED, LOW);      			// we blink the LED for a bit as a visual indicator of the knock.
+  if (programButtonPressed==true){
+     digitalWrite(redLED, LOW);                         // and the red one too if we're programming a new knock.
+  }
+  delay(knockFadeTime);                       	        // wait for this peak to fade before we listen to the next one.
+  digitalWrite(greenLED, HIGH);  
+  if (programButtonPressed==true){
+     digitalWrite(redLED, HIGH);                        
+  }
+  do {
+    //listen for the next knock or wait for it to timeout. 
+    knockSensorValue = analogRead(knockSensor);
+    if (knockSensorValue >=threshold){                   //got another knock...
+      //record the delay time.
+      Serial.println("knock.");
+      now=millis();
+      knockReadings[currentKnockNumber] = now-startTime;
+      currentKnockNumber ++;                             //increment the counter
+      startTime=now;          
+      // and reset our timer for the next knock
+      digitalWrite(greenLED, LOW);  
+      if (programButtonPressed==true){
+        digitalWrite(redLED, LOW);                       // and the red one too if we're programming a new knock.
+      }
+      delay(knockFadeTime);                              // again, a little delay to let the knock decay.
+      digitalWrite(greenLED, HIGH);
+      if (programButtonPressed==true){
+        digitalWrite(redLED, HIGH);                         
+      }
+    }
+
+    now=millis();
+    
+    //did we timeout or run out of knocks?
+  } while ((now-startTime < knockComplete) && (currentKnockNumber < maximumKnocks));
+  
+  //we've got our knock recorded, lets see if it's valid
+  if (programButtonPressed==false){             // only if we're not in progrmaing mode.
+    if (validateKnock() == true && open == false){
+      triggerDoorUnlock();
+      //Play the song after opening
+      playSong();
+    } else {
+      Serial.println("Secret knock failed.");
+      digitalWrite(greenLED, LOW);  		// We didn't unlock, so blink the red LED as visual feedback.
+      for (i=0;i<4;i++){					
+        digitalWrite(redLED, HIGH);
+        tone(buzzer, 250);
+        delay(100);
+        digitalWrite(redLED, LOW);
+        noTone(buzzer);  
+        //digitalWrite(buzzer, LOW);
+        delay(100);
+      }
+      digitalWrite(greenLED, HIGH);
+    }
+  } else { // if we're in programming mode we still validate the lock, we just don't do anything with the lock
+    validateKnock();
+    // and we blink the green and red alternately to show that program is complete.
+    Serial.println("New lock stored.");
+    digitalWrite(redLED, LOW);
+    digitalWrite(greenLED, HIGH);
+    for (i=0;i<3;i++){
+      delay(100);
+      digitalWrite(redLED, HIGH);
+      digitalWrite(greenLED, LOW);
+      delay(100);
+      digitalWrite(redLED, LOW);
+      digitalWrite(greenLED, HIGH);      
+    }
+  }
+}
+
+// Runs the motor to unlock the door.
+void triggerDoorUnlock(){
+  Serial.println("Door unlocked!");
+  int i=0;
+  
+  // turn the motor on for a bit.
+  lockMotor.step(1024);   //Turn the stepper 180 degrees to open
+  
+  open = true;
+
+  // Blink the green LED a few times for more visual feedback.
+  for (i=0; i < 5; i++){   
+      digitalWrite(greenLED, LOW);
+      delay(100);
+      digitalWrite(greenLED, HIGH);
+      delay(100);
+  }
+}
+
+void doorLock() {
+  Serial.println("Door locked!");
+
+  lockMotor.step(-1024);   //Turn the stepper 180 degrees to open
+
+  open = false;
+}
+
+void playSong() {
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(buzzer);
+  }
+}
+// Sees if our knock matches the secret.
+// returns true if it's a good knock, false if it's not.
+// todo: break it into smaller functions for readability.
+boolean validateKnock(){
+  int i=0;
+ 
+  // simplest check first: Did we get the right number of knocks?
+  int currentKnockCount = 0;
+  int secretKnockCount = 0;
+  int maxKnockInterval = 0;          			// We use this later to normalize the times.
+  
+  for (i=0;i<maximumKnocks;i++){
+    if (knockReadings[i] > 0){
+      currentKnockCount++;
+    }
+    if (secretCode[i] > 0){  					//todo: precalculate this.
+      secretKnockCount++;
+    }
+    
+    if (knockReadings[i] > maxKnockInterval){ 	// collect normalization data while we're looping.
+      maxKnockInterval = knockReadings[i];
+    }
+  }
+  
+  // If we're recording a new knock, save the info and get out of here.
+  if (programButtonPressed==true){
+      for (i=0;i<maximumKnocks;i++){ // normalize the times
+        secretCode[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100); 
+      }
+      // And flash the lights in the recorded pattern to let us know it's been programmed.
+      digitalWrite(greenLED, LOW);
+      digitalWrite(redLED, LOW);
+      delay(1000);
+      digitalWrite(greenLED, HIGH);
+      digitalWrite(redLED, HIGH);
+      delay(50);
+      for (i = 0; i < maximumKnocks ; i++){
+        digitalWrite(greenLED, LOW);
+        digitalWrite(redLED, LOW);  
+        // only turn it on if there's a delay
+        if (secretCode[i] > 0){                                   
+          delay( map(secretCode[i],0, 100, 0, maxKnockInterval)); // Expand the time back out to what it was.  Roughly. 
+          digitalWrite(greenLED, HIGH);
+          digitalWrite(redLED, HIGH);
+        }
+        delay(50);
+      }
+	  return false; 	// We don't unlock the door when we are recording a new knock.
+  }
+  
+  if (currentKnockCount != secretKnockCount){
+    return false; 
+  }
+  
+  /*  Now we compare the relative intervals of our knocks, not the absolute time between them.
+      (ie: if you do the same pattern slow or fast it should still open the door.)
+      This makes it less picky, which while making it less secure can also make it
+      less of a pain to use if you're tempo is a little slow or fast. 
+  */
+  int totaltimeDifferences=0;
+  int timeDiff=0;
+  for (i=0;i<maximumKnocks;i++){ // Normalize the times
+    knockReadings[i]= map(knockReadings[i],0, maxKnockInterval, 0, 100);      
+    timeDiff = abs(knockReadings[i]-secretCode[i]);
+    if (timeDiff > rejectValue){ // Individual value too far out of whack
+      return false;
+    }
+    totaltimeDifferences += timeDiff;
+  }
+  // It can also fail if the whole thing is too inaccurate.
+  if (totaltimeDifferences/secretKnockCount>averageRejectValue){
+    return false; 
+  }
+  
+  return true;
+  
+}
